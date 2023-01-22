d2d676bd3dd1d237c3443e398d92aff6f6442d1f
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-01-22 20:14:23 +0530

    advancedJavascript Task9

---
+++ advancedJavascript/indexTask9.js
@@ -0,0 +1,21 @@
+// let multiply = function (x, y) {
+//     console.log(x * y);
+// }
+
+let multiply = function (x) {
+    return function (y) {
+        console.log(x * y);
+    }
+}
+
+// let multiplyByTwo = multiply.bind(this, 2)
+// multiplyByTwo(3);
+
+// let multiplyByThree = multiply.bind(this, 3)
+// multiplyByThree(5);
+
+let multiplyByTwo = multiply(2);
+multiplyByTwo(3);
+
+let multiplyByThree = multiply(10);
+multiplyByThree(3);
